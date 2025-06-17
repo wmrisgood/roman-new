203570781fd7e78b70d99ad86a33dcff2a37163c
efotacel <efotacel@email1.io>
2025-06-17 09:20:33 -0700

    final

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ document.getElementById('hamburger').addEventListener('click', () => {
   document.querySelector('.bar2').classList.toggle('bar2-transform');
   document.querySelector('.bar3').classList.toggle('bar3-transform');
 });
+
+let carouselIndex = 0;
+let carouselImages = [
+  'images/hof-1.png', 'images/hof-2.png', 'images/hof-3.png', 'images/hof-4.png'
+];
+let len = carouselImages.length
+
+document.getElementById('btn-left').addEventListener('click', () => {
+  carouselIndex += len - 1;
+  document.getElementById('carousel-image').src =
+      carouselImages[carouselIndex % len];
+});
+
+document.getElementById('btn-right').addEventListener('click', () => {
+  carouselIndex += 1;
+  document.getElementById('carousel-image').src =
+      carouselImages[carouselIndex % len];
+});
